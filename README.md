344adf7541560a90b0f99acffc930505a4410fbd
Adam Molnar <adam@molnar.click>
2025-02-15 17:02:48 -0500

    RAG using web resource

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# LLM_Scripts
- 
+# LLM Python Scripts
+
+Testing local LLMs using [Ollama](https://github.com/ollama/ollama) via Docker container using the [LangChain](https://python.langchain.com/) framework library and using the [DeepSeek-R1](https://github.com/deepseek-ai/DeepSeek-R1) model.
+
+### Scripts:
+
+- **rag_webbase.py**
+Demonstrates RAG (Retrieval-Augmented Generation) which uses a [blog post](https://lilianweng.github.io/posts/2023-06-23-agent/) by Lilian Weng on agents as an example.
+
+### Requirements
+
+- Docker for Windows
+- Windows Subsystem for Linux (WSL) if you are using a GPU
+- VS Code
+
+### Setup
+
+- Clone repo and open folder in VS Code.
+- Right click the Dockerfile in the Docker folder and Build Image...
+- Run container setting a volume pointing to Scripts directory for developing/testing scripts
+`docker run -d --gpus=all -v ollama:/root/.ollama -v "%userprofile%\Documents\GitHub\LLM_Scripts\Scripts":/Scripts -p 11434:11434 --name llmscripts-main llmscripts:latest`
+- Pull models, these get stored in the containers volume
+`docker exec -it llmscripts-main ollama pull nomic-embed-text`
+`docker exec -it llmscripts-main ollama pull deepseek-r1`
+- Now you can use bash to execute python scripts, located in /Scripts folder
+`docker exec -it llmscripts-main bash`
+`root@32daf2b4f53a:/Scripts# python3 rag_webbase.py`
+
+### Resources
+- [Build a Local RAG Application](https://python.langchain.com/v0.2/docs/tutorials/local_rag/)
---
+++ Scripts/rag_webbase.py
@@ -0,0 +1,49 @@
+from langchain_community.document_loaders import WebBaseLoader # type: ignore
+from langchain_text_splitters import RecursiveCharacterTextSplitter # type: ignore
+
+loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+from langchain_chroma import Chroma # type: ignore
+from langchain_ollama import OllamaEmbeddings # type: ignore
+
+local_embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=local_embeddings)
+
+question = "What are the approaches to Task Decomposition?"
+docs = vectorstore.similarity_search(question)
+
+from langchain_ollama import ChatOllama # type: ignore
+
+model = ChatOllama(
+    model="deepseek-r1",
+)
+
+from langchain_core.output_parsers import StrOutputParser # type: ignore
+from langchain_core.prompts import ChatPromptTemplate # type: ignore
+
+prompt = ChatPromptTemplate.from_template(
+    "Summarize the main themes in these retrieved docs: {docs}"
+)
+
+# Convert loaded documents into strings by concatenating their content
+# and ignoring metadata
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+chain = {"docs": format_docs} | prompt | model | StrOutputParser()
+
+question = "What are the approaches to Task Decomposition?"
+
+docs = vectorstore.similarity_search(question)
+
+print(chain.invoke(docs))
